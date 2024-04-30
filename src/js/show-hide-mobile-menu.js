5a98c3161383de23cd6f7f554bc05f679bd98a59
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-04-30 23:21:38 +0200

    Add some sections

---
+++ src/js/show-hide-mobile-menu.js
@@ -0,0 +1,13 @@
+const buttonMenuRef = document.querySelector('.js-open-mobile-menu-button');
+const mobileMenuRef = document.querySelector('.js-mobile-menu');
+const buttonCloseMenuRef = document.querySelector(
+  '.js-close-mobile-menu-button'
+);
+
+buttonMenuRef.addEventListener('click', () => {
+  mobileMenuRef.classList.add('is-open');
+});
+
+buttonCloseMenuRef.addEventListener('click', () => {
+  mobileMenuRef.classList.remove('is-open');
+});
