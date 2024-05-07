a9ea6bbf1c2543151628a46be7a92ec4e59300eb
aandrea-alex <151180549+aandrea-alex@users.noreply.github.com>
2024-05-07 19:07:03 +0200

    add picture

--- src/js/footer-scroll-handler.js
+++ src/js/footer-scroll-handler.js
@@ -1,18 +1,15 @@
-import { onScrollUp, onScrollToElement } from "./scroll";
-import { removeActiveClass, addActiveClass } from "./menu-item";
+import { onScrollUp, onScrollToElement } from './scroll';
 
 const scrollUpBtn = document.querySelector('.js-footer-scroll');
-scrollUpBtn && scrollUpBtn.addEventListener("click", onScrollUp);
+scrollUpBtn && scrollUpBtn.addEventListener('click', onScrollUp);
 
 const footerMenuRef = document.querySelector('.js-footer-menu');
 
 footerMenuRef &&
-footerMenuRef.addEventListener('click', event => {
+  footerMenuRef.addEventListener('click', event => {
     const item = event.target;
     if (!item.matches('a')) return;
-    event.preventDefault();  
-    removeActiveClass();
-    addActiveClass(item); 
+    event.preventDefault();
     const targetId = item.getAttribute('href').substring(1);
     onScrollToElement(targetId);
   });
